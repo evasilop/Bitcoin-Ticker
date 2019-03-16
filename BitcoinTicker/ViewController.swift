9fa9b68a0393ef998e571f1c20bdc932f1f9ba03
Efthimios Vasilopoulos <efthi@Efthimioss-MacBook-Pro.local>
2019-03-16 17:42:21 +0100

    Configuration of Parsing and Ne

--- BitcoinTicker/ViewController.swift
+++ BitcoinTicker/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
     let currencyArray =  ["AUD","BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    let currencySymbol = ["$", "R$", "$", "¥", "€", "£", "$", "Rp", "₪", "₹", "¥", "$", "kr", "$", "zł", "lei", "₽", "kr", "$", "$", "R"]
+    
+    var selectedSymbol = ""
     var finalURL = ""
 
     //Pre-setup IBOutlets
@@ -50,58 +54,53 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         
         finalURL = baseURL + currencyArray[row]
         print(finalURL)
+        selectedSymbol = currencySymbol[row]
+        getCurrentPrice(url: finalURL)
+        
     }
     
 
     
     
+    //MARK: - Networking
+    /***************************************************************/
     
-//    
-//    //MARK: - Networking
-//    /***************************************************************/
-//    
-//    func getWeatherData(url: String, parameters: [String : String]) {
-//        
-//        Alamofire.request(url, method: .get, parameters: parameters)
-//            .responseJSON { response in
-//                if response.result.isSuccess {
-//
-//                    print("Sucess! Got the weather data")
-//                    let weatherJSON : JSON = JSON(response.result.value!)
-//
-//                    self.updateWeatherData(json: weatherJSON)
-//
-//                } else {
-//                    print("Error: \(String(describing: response.result.error))")
-//                    self.bitcoinPriceLabel.text = "Connection Issues"
-//                }
-//            }
-//
-//    }
-//
-//    
-//    
-//    
-//    
-//    //MARK: - JSON Parsing
-//    /***************************************************************/
-//    
-//    func updateWeatherData(json : JSON) {
-//        
-//        if let tempResult = json["main"]["temp"].double {
-//        
-//        weatherData.temperature = Int(round(tempResult!) - 273.15)
-//        weatherData.city = json["name"].stringValue
-//        weatherData.condition = json["weather"][0]["id"].intValue
-//        weatherData.weatherIconName =    weatherData.updateWeatherIcon(condition: weatherData.condition)
-//        }
-//        
-//        updateUIWithWeatherData()
-//    }
-//    
+    func getCurrentPrice(url: String) {
+        
+        Alamofire.request(url, method: .get)
+            .responseJSON { response in
+                if response.result.isSuccess {
 
+                    print("Sucess! Got the Current Price")
+                    let priceJSON : JSON = JSON(response.result.value!)
 
+                    self.updateCurrentPrice(json: priceJSON)
 
+                } else {
+                    print("Error: \(String(describing: response.result.error))")
+                    self.bitcoinPriceLabel.text = "Connection Issues"
+                }
+            }
+    }
 
+    
+    
+    
+    
+    //MARK: - JSON Parsing
+    /***************************************************************/
+    
+    func updateCurrentPrice(json : JSON) {
+        
+        if let tempPrice = json["ask"].double {
+            print(tempPrice)
+        bitcoinPriceLabel.text = "\(selectedSymbol)\(tempPrice)"
+        
+        } else {
+            bitcoinPriceLabel.text = "Parsing Issues"
+        }
+        
+        
+    }
 }
 
